10deee56e311c28ff4c22e9e336146b73d9cb6bb
Gonçalo Azevedo <goncalobmaz@gmail.com>
2023-09-25 15:21:05 +0100

    build expandable text

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "./pages/layout";
 import Homepage from "./pages/Homepage";
 import GameDetailPage from "./pages/GameDetailPage";
 import ErrorPage from "./pages/ErrorPage";
+import Layout from "./pages/Layout";
 
 const router = createBrowserRouter([
   {
